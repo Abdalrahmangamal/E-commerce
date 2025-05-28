174bdc039f2910fc713d4623113264db05e407ed
Your Name <your.email@example.com>
2025-05-28 17:47:48 +0300

    complete fitch category

--- src/Components/Slideproducts/Slideproduct.jsx
+++ src/Components/Slideproducts/Slideproduct.jsx
@@ -1,51 +1,16 @@
-import React, { useState } from "react";
 import ProductCard from "./ProductCard";
 import "./slideproduct.css";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Autoplay,Navigation } from "swiper/modules";
+import { Autoplay, Navigation } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
-import { useEffect } from "react";
 
-
-const categories =[
-  "smartphones",
-  "mobile-accessories",
-  "laptops",
-  "tablets",
-  "sunglasses",
-  "sports-accessories"
-]
-
-export default function Slideproduct() {
-  
-  const [products,setproducts]=useState({})
-  useEffect(()=>{
-
-    const fetchproducts = async () =>{
-      try{
-        const results = await Promise.all(
-          categories.map(async (category)=>{
-            const res =await fetch(`https://dummyjson.com/products/category/${category}`);
-            const data = await res.json();
-            return{[category]:data.products}
-          })
-        )
-        const productsData = Object.assign({},...results);
-        setproducts(productsData)
-
-      } catch(error){
-        console.error("error")
-      }
-    }
-    fetchproducts()
-  },[])
-
+export default function Slideproduct({title}) {
   return (
     <div className="slide_products slide">
       <div className="container">
         <div className="top_slide">
-          <h2>Lorem ipsum dolor sit amet.</h2>
+          <h2>{title}</h2>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi,
             reiciendis?
@@ -62,21 +27,6 @@ export default function Slideproduct() {
           className="mySwiper"
           loop={true}
         >
-          <SwiperSlide>
-            <ProductCard />
-          </SwiperSlide>
-          <SwiperSlide>
-            <ProductCard />
-          </SwiperSlide>
-          <SwiperSlide>
-            <ProductCard />
-          </SwiperSlide>
-          <SwiperSlide>
-            <ProductCard />
-          </SwiperSlide>
-          <SwiperSlide>
-            <ProductCard />
-          </SwiperSlide>
           <SwiperSlide>
             <ProductCard />
           </SwiperSlide>
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,48 @@
-import React from 'react'
-import Heroslider from '../../Components/Home/Heroslider'
-import './home.css'
-import Slideproduct from '../../Components/Slideproducts/Slideproduct'
+import Heroslider from "../../Components/Home/Heroslider";
+import "./home.css";
+import Slideproduct from "../../Components/Slideproducts/Slideproduct";
+import { useState, useEffect } from "react";
+
+const categories = [
+  "smartphones",
+  "mobile-accessories",
+  "laptops",
+  "tablets",
+  "sunglasses",
+  "sports-accessories",
+];
+
 export default function Home() {
+  const [products, setproducts] = useState({});
+  useEffect(() => {
+    const fetchproducts = async () => {
+      try {
+        const results = await Promise.all(
+          categories.map(async (category) => {
+            const res = await fetch(
+              `https://dummyjson.com/products/category/${category}`
+            );
+            const data = await res.json();
+            return { [category]: data.products };
+          })
+        );
+        const productsData = Object.assign({}, ...results);
+        setproducts(productsData);
+      } catch (error) {
+        console.error("error");
+      }
+    };
+    fetchproducts();
+  }, []);
+  console.log(products);
+
   return (
     <div>
-      <Heroslider/>
-      <Slideproduct/>
+      <Heroslider />
+{categories.map((category)=>(
+  
+  <Slideproduct data={products[category]} title={category}  />
+))}
     </div>
-  )
+  );
 }
